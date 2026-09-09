640a67001bf8cd7c941c2b0f7ca15fed49605f0a
mac-mac-sg <marco.raith@outlook.com>
2026-09-09 22:02:49 +0200

    Apply smartphone view-mode CSS overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './focus.css'
 import './mobile.css'
+import './focus-mobile.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
