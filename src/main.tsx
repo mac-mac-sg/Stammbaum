426b5b6f6bb9b7b810a16c856dfe58ad4f700d5a
mac-mac-sg <marco.raith@outlook.com>
2026-09-10 10:36:35 +0200

    Wire home navigation provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { EditProvider } from './EditContext'
+import { FamilyNavigationProvider } from './FamilyNavigationContext'
 import { PrivacyProvider } from './PrivacyContext'
 import { registerServiceWorker } from './registerServiceWorker'
 import { assertValidGenealogy } from './dataValidation'
@@ -17,6 +18,7 @@ import './edit.css'
 import './corrections.css'
 import './partner-person.css'
 import './tree-couples.css'
+import './home.css'
 
 assertValidGenealogy(people, peopleById, rootId)
 
@@ -24,7 +26,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <EditProvider>
       <PrivacyProvider>
-        <App />
+        <FamilyNavigationProvider>
+          <App />
+        </FamilyNavigationProvider>
       </PrivacyProvider>
     </EditProvider>
   </React.StrictMode>,
