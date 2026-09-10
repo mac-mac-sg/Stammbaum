1d2eab9ede6ac31c5b650f61f30f8e55839cca8c
mac-mac-sg <marco.raith@outlook.com>
2026-09-10 12:29:20 +0200

    Add Michelle Herren as partner of Marco Raith

--- src/data/people-90-104.ts
+++ src/data/people-90-104.ts
@@ -6,7 +6,7 @@ export const people90to104: Person[] = [
   {"id":"p092","number":92,"name":"Roxana Maurer","birth":"1999-06-09","birthPlace":"Uster","gender":"f","source":8,"generation":5,"parentId":"p057","childIds":[],"partners":[]},
   {"id":"p093","number":93,"name":"Nadine Jerg","birth":"1986-06-05","birthPlace":"Goldach / Frauenfeld","gender":"f","source":8,"generation":5,"parentId":"p058","childIds":[],"partners":[]},
   {"id":"p094","number":94,"name":"Stefanie Jerg","birth":"1990-09-25","birthPlace":"Goldach","gender":"f","source":8,"generation":5,"parentId":"p058","childIds":[],"partners":[]},
-  {"id":"p095","number":95,"name":"Marco Raith","birth":"1990-04-21","birthPlace":"Goldach / SG","gender":"m","source":7,"generation":5,"parentId":"p059","childIds":[],"partners":[]},
+  {"id":"p095","number":95,"name":"Marco Raith","birth":"1990-04-21","birthPlace":"Goldach / SG","gender":"m","source":7,"generation":5,"parentId":"p059","childIds":[],"partners":[{"name":"Michelle Herren","birth":"1991-01-21","birthPlace":"Schaffhausen","relationship":"Partnerschaft"}]},
   {"id":"p096","number":96,"name":"Sandro Raith","birth":"1992-07-11","birthPlace":"Goldach / SG","death":"2010-05-15","gender":"m","source":7,"generation":5,"parentId":"p059","childIds":[],"partners":[]},
   {"id":"p097","number":97,"name":"Nico Jerg","birth":"2006-10-20","birthPlace":"Goldach","gender":"m","source":7,"generation":5,"parentId":"p060","childIds":[],"partners":[]},
   {"id":"p098","number":98,"name":"Giulia Jerg","birth":"2008-06-13","birthPlace":"Egnach","gender":"f","source":7,"generation":5,"parentId":"p061","childIds":[],"partners":[]},
