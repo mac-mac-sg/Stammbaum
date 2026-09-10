c4c15951ba37debf0b83bf407ef121bc6b874e53
mac-mac-sg <marco.raith@outlook.com>
2026-09-10 06:42:38 +0200

    Load correction data tool styles

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './relationship.css'
 import './privacy.css'
 import './mobile-search.css'
 import './edit.css'
+import './corrections.css'
 
 assertValidGenealogy(people, peopleById, rootId)
 
